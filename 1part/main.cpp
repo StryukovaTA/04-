478fbaf765575137bd320b613283f0fdbf267272
StryukovaTA <79773436169@yandex.ru>
2019-06-03 21:51:43 +0300

    code : 1.4

---
+++ 1part/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+#include <cassert>
+
+using namespace std;
+
+
+char nibble_to_hex(uint8_t i)
+{
+    assert(0x0 <= i && i <= 0xf);
+    char digits[]="0123456789abcdef";
+    return digits[i];
+}
+
+
+void
+print_in_hex(uint8_t byte)
+{
+    cout << nibble_to_hex(byte >> 4)
+         << nibble_to_hex(byte & 0xf);
+}
+
+int main()
+{
+    assert(nibble_to_hex(0x0) == '0');
+    assert(nibble_to_hex(0x1) == '1');
+    assert(nibble_to_hex(0x2) == '2');
+    assert(nibble_to_hex(0x3) == '3');
+    assert(nibble_to_hex(0x4) == '4');
+    assert(nibble_to_hex(0x5) == '5');
+    assert(nibble_to_hex(0x6) == '6');
+    assert(nibble_to_hex(0x7) == '7');
+    assert(nibble_to_hex(0x8) == '8');
+    assert(nibble_to_hex(0x9) == '9');
+    assert(nibble_to_hex(0xa) == 'a');
+    assert(nibble_to_hex(0xb) == 'b');
+    assert(nibble_to_hex(0xc) == 'c');
+    assert(nibble_to_hex(0xd) == 'd');
+    assert(nibble_to_hex(0xe) == 'e');
+    assert(nibble_to_hex(0xf) == 'f');
+    print_in_hex(0x0);
+    print_in_hex(0xab);
+    print_in_hex(0xff);
+    getch();
+    return 0;
+}
