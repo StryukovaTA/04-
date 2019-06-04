a4d558990ff86cc73b25da2d45bd5f893af8eee4
StryukovaTA <79773436169@yandex.ru>
2019-06-04 11:12:27 +0300

    code : строки и описание функций

--- string/main.cpp
+++ string/main.cpp
@@ -15,42 +15,36 @@ int main()
     fgets(text, MAX_SIZE, stdin);
     char* filename = text;
     filename[strlen(filename) - 1] = '\0';  //strlen возвращает в байтах длину строки
-    while(true)
-    {
-        if( strcspn( filename, forbidden ) == strlen( filename ) ) ; //Длина начального участка строки, не содержащая символов указанных в аргументе
-        else
-        {
+    while(true) {
+        if( strcspn( filename, forbidden ) == strlen( filename ) ) ;//Длина начального участка строки, не содержащая символов указанных в аргументе
+        else {
             cout << "Invalid format";
             break;
         }
 
-        if( strchr( filename, ':' ) == nullptr ) ;
-        else
-        {
-            char* colon = strchr(filename,':');//strchr выполняет поиск первого вхождения символа : в filename
-            if( colon - filename + 1 <= 1 )
-            {
+        if( strchr( filename, ':' ) == nullptr ) ;  //strchr выполняет поиск первого вхождения символа : в filename
+        else {
+            char* colon = strchr(filename,':');
+            if( colon - filename + 1 <= 1 ) {
                 cout << "Invalid format";
                 break;
             }
-            while( colon != nullptr )
-            {
+            while( colon != nullptr ) {
                 if( isalpha( filename[colon - filename - 1] ) > 0 && //isalpha() возвращает ненулевое значение, если его аргумент является буквой алфавита (верхнего или нижнего регистра). В противном случае возвращается 0.
-                        filename[colon - filename + 1] == '\\')
+                    filename[colon - filename + 1] == '\\')
                     colon = strchr(colon + 1, ':');
-                else
-                {
+                else {
                     cout << "Invalid format";
                     break;
                 }
             }
         }
 
-        if( strrchr( filename, '.' ) == nullptr )//ищет последнее вхождение символа
+        if( strrchr( filename, '.' ) == nullptr ) //ищет последнее вхождение символа
             strcat( filename, ".txt" ); //strcat() присоединяет к строке str1 копию строки str2 и завершает строку str1 нулевым символом
 
-        else
-        {
+
+        else {
             char* ext = strrchr( filename, '.' );
             for( int i = 1; ext[i] != '\0'; i ++ )
                 *(ext + i) = char(tolower(*(ext + i)));
@@ -58,26 +52,31 @@ int main()
 //0 – если первые n символов сравниваемых строк идентичны.
 //Положительное число – если в первых n символах сравниваемых строк есть отличия и код первого отличающегося символа в строке str1 больше кода символа на той же позиции в строке str2.
 //Отрицательное число – в первых n символах сравниваемых строк есть отличия и код первого отличающегося символа в строке str1 меньше кода символа на той же позиции в строке str2.
-            else
-            {
+
+            else {
                 cout << "Invalid format";
                 break;
             }
         }
 
-        FILE* file = fopen( filename, "r" );//Создает файл для записи (по умолчанию файл открывается как текстовый).
-        if( file == nullptr )
-        {
+        puts(filename);
+
+        FILE* file = fopen( filename, "r" ); //Создает файл для записи (по умолчанию файл открывается как текстовый).
+
+        if( file == nullptr ) {
             cout << "Invalid file name";
             break;
         }
         fseek( file, 0, SEEK_END );//fseek перемещает указатель позиции в потоке. Устанавливает внутренний указатель положения в файле, в новую позицию, которая определяются путем добавления смещения к исходному положению.
+
         long filesize = ftell( file );//ftell – опрос текущей позиции в потоке данных
 
+
         char* filedata = new char[filesize];
-        rewind( file );//станавливает внутренний указатель положения файла в начальное положение (начало файла).
-        fread( filedata, 1, filesize, file );
-        filedata[filesize] = '\0';
+        rewind( file ); //станавливает внутренний указатель положения файла в начальное положение (начало файла).
+        fread( filedata, 1, filesize, file ); //fread считывает массив размером — count элементов, каждый из которых имеет размер size байт, из потока, и сохраняет его в блоке памяти, на который указывает ptrvoid.
+        char* fldt = filedata;
+        fldt[filesize  ] = '\0';
 
         char STR[MAX_SIZE];
         cout << "Enter string: ";
@@ -85,23 +84,15 @@ int main()
         char* str = STR;
         *( str + strlen(str) - 1 ) = '\0';
 
-
         int occur = 0;
-        while( *filedata != '\0'  )
-        {
-            if( strncmp( filedata, str, strlen(str) ) != 0 )
-                filedata += 1;
-            else
-            {
-                occur ++;
-                filedata += 1;
-            }
-            cout << "Occurance number: " << occur;
-
-            delete[] filedata;
-            fclose( file );
-            break;
+        while( strstr( fldt, str ) != nullptr ) { //strstr – поиск первого вхождения строки А в строку В
+            occur ++;
+            fldt = strstr( fldt, str ) + 1;
         }
-        return 0;
+        cout << "Occurance number: " << occur;
+        delete[] filedata;
+        fclose( file );
+        break;
     }
+    return 0;
 }
